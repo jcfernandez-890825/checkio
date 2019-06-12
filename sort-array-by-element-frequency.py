773647a2d5852b2ef33ad235642038a247325a6b
Juan Carlos <juan.carlos.fdez.89@gmail.com>
2019-06-12 15:09:07 -0400

    Update sort-array-by-element-frequency.py

--- sort-array-by-element-frequency.py
+++ sort-array-by-element-frequency.py
@@ -2,6 +2,16 @@ def frequency_sort(items):
     # your code here
     return sorted(items, key=lambda i: (items.count(i), -1 * items.index(i)), reverse=True)
 
+'''
+Chekio Mission Link:
+https://py.checkio.org/en/mission/sort-array-by-element-frequency/
+
+Chekio Solution Link:
+https://py.checkio.org/mission/sort-array-by-element-frequency/publications/jcfernandez/python-3/only-one-line/share/b925502bb310020ce2dd7314ec038769/
+
+Chekio Profile Link:
+https://py.checkio.org/user/jcfernandez/solutions/
+'''
 
 if __name__ == '__main__':
     print("Example:")
