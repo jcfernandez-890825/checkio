50b911962abf4e90d7296ea880bb96b5a623d0aa
Juan Carlos <juan.carlos.fdez.89@gmail.com>
2019-06-12 15:57:43 -0400

    Update all-the-same.py

--- all-the-same.py
+++ all-the-same.py
@@ -5,6 +5,17 @@ def all_the_same(elements_lst: List[Any]) -> bool:
     return len(set(elements_lst)) <= 1
 
 
+'''
+Chekio Mission Link:
+https://py.checkio.org/en/mission/sort-array-by-element-frequency/
+
+Chekio Solution Link:
+https://py.checkio.org/mission/all-the-same/publications/jcfernandez/python-3/first/share/75208635e985fd18291ff5ac8d35b6e6/
+
+Chekio Profile Link:
+https://py.checkio.org/user/jcfernandez/solutions/
+'''
+
 if __name__ == '__main__':
     print("Example:")
     print(all_the_same([1, 1, 1]))
